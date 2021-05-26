a52cec8ff35a25959ecf17b500ccf2143365d4f7
selaytekgul <selay.tekgul@tedu.edu.tr>
2021-05-26 23:17:04 +0300

    check json req&res

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(express.static(path.join(__dirname, "public")));
 // to handle CORS ERROR
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");//allow client to which methods to use
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization"); //allow client to which headers to set
   next();
 });
 
--- controllers/user.js
+++ controllers/user.js
@@ -13,9 +13,10 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
+  const toAnalyze=req.body.toAnalyze;
   // Create post in db
   res.status(201).json({
     message: "Post created successfully!",
-    post: { id: new Date().toISOString(), title: title, content: content },
+    post: { id: new Date().toISOString(), title: title, content: content ,toAnalyze:toAnalyze},
   });
 };
