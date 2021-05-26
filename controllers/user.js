7127ebbcbc59e2df1aec07a34b12ec40572b8645
selaytekgul <selay.tekgul@tedu.edu.tr>
2021-05-26 15:01:47 +0300

    gitignore added & post try

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,21 @@
 exports.getUser = (req, res, next) => {
   res.status(200).json({ message: "user" });
 };
-exports.getEmotion = (req, res, next) => {
-    res.status(200).json({ message: "/user/emotion" });
+exports.getOcean = (req, res, next) => {
+  res.status(200).json({ message: "/user/ocean" });
+};
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [{ title: "First Post", content: "This is the first post!" }],
+  });
+};
 
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in db
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: { id: new Date().toISOString(), title: title, content: content },
+  });
 };
--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,12 @@ var router = express.Router();
 /* GET users listing. */
 router.get("/", userController.getUser);
 
-router.get("/emotion", userController.getEmotion);
+router.get("/ocean", userController.getOcean);
+
+// GET /feed/posts
+router.get('/posts', userController.getPosts);
+
+// POST /feed/post
+router.post('/post', userController.createPost);
 
 module.exports = router;
