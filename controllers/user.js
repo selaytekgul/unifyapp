bf8b9e496c0cd86b2c8847d83a233d7396b84aee
selaytekgul <selay.tekgul@tedu.edu.tr>
2021-05-30 00:59:50 +0300

    IBM NLU test 13 Obtain Categories

--- controllers/user.js
+++ controllers/user.js
@@ -56,52 +56,61 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = async (req, res, next) => {
+  const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1");
+  const { IamAuthenticator } = require("ibm-watson/auth");
+
+  const naturalLanguageUnderstanding = await new NaturalLanguageUnderstandingV1(
+    {
+      version: "2020-08-01",
+      authenticator: new IamAuthenticator({
+        apikey: "zh2ta0pUk00isPtZl4tuv3rHU2JRDyhA2-IZl68P6VxH",
+      }),
+      serviceUrl:
+        "https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/a5181c10-50ed-4788-8797-ef900d41c5ea",
+    }
+  );
+
+  const analyzeParams = {
+    text: req.body.toAnalyze,
+    features: {
+      keywords: {
+        emotion: true,
+      },
+      categories: {
+        emotion: true,
+        sentiment: true,
+      },
+    },
+  };
   try {
-    const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1");
-    const { IamAuthenticator } = require("ibm-watson/auth");
-
-    const naturalLanguageUnderstanding =
-      await new NaturalLanguageUnderstandingV1({
-        version: "2020-08-01",
-        authenticator: new IamAuthenticator({
-          apikey: "zh2ta0pUk00isPtZl4tuv3rHU2JRDyhA2-IZl68P6VxH",
-        }),
-        serviceUrl:
-          "https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/a5181c10-50ed-4788-8797-ef900d41c5ea",
-      });
-
-    const toAnalyze = req.body.toAnalyze;
     const analysisResults = await naturalLanguageUnderstanding.analyze(
-      toAnalyze
+      analyzeParams
     );
 
-    categories = [];
     if (analysisResults["status"] == 200) {
-      for (const key in analysisResults["result"]) {
-        for (
-          let index = 0;
-          index < analysisResults["result"][key].length;
-          index++
-        ) {
-          const element = analysisResults["result"][key][index];
-
-          if (element["label"] != null) {
-            var category = element["label"].substr(1).split("/");
-            for (let j = 0; j < category.length; j++) {
-              categories.push(category[j]);
-            }
+      for (const key in analysisResults) {
+        console.log(key);
+        if (key == "result") {
+          for (let i = 0; i < key.length; i++) {
+            const element = key[i];
+            console.log(element);
           }
         }
       }
 
-      let categoriesUnique = categories.filter(
-        (value, index, categories) => categories.indexOf(value) === index
-      );
       res.status(201).json({
-        result: JSON.stringify(categoriesUnique),
+        result: analysisResults,
+      });
+    } else {
+      res.status(analysisResults["status"]).json({
+        result: analysisResults["statusText"],
       });
     }
   } catch (error) {
     console.log("error:", error);
+    res.status(error["status"]).json({
+      response: "Error Occured",
+      reason: error["name"],
+    });
   }
 };
