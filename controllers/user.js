80128c9814c1c45ae96825775894ff7e43ed5f48
selaytekgul <selay.tekgul@tedu.edu.tr>
2021-05-31 23:11:51 +0300

    Get User's Tweets w/ User Name and Analyze Them and Return Unique Categories

--- controllers/user.js
+++ controllers/user.js
@@ -87,3 +87,187 @@ exports.createPost = async (req, res, next) => {
     });
   }
 };
+exports.getUserCategoryFromTweets = async (req, res, next) => {
+  try {
+    const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1");
+    const { IamAuthenticator } = require("ibm-watson/auth");
+    const needle = require("needle");
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+      version: "2020-08-01",
+      authenticator: new IamAuthenticator({
+        apikey: "zh2ta0pUk00isPtZl4tuv3rHU2JRDyhA2-IZl68P6VxH",
+      }),
+      serviceUrl:
+        "https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/a5181c10-50ed-4788-8797-ef900d41c5ea",
+    });
+
+    // https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/master/User-Lookup/get_users_with_bearer_token.js
+    // Get User objects by username, using bearer token authentication
+    // https://developer.twitter.com/en/docs/twitter-api/users/lookup/quick-start
+
+    // The code below sets the bearer token from your environment variables
+    // To set environment variables on macOS or Linux, run the export command below from the terminal:
+    // export BEARER_TOKEN='YOUR-TOKEN'
+    const token = process.env.BEARER_TOKEN;
+    const endpointURL = "https://api.twitter.com/2/users/by?usernames=";
+
+    // specify User names to fetch, and any additional fields that are required
+    // by default, only the User ID, name and user name are returned
+    const paramsUserID = {
+      usernames: req.body.userName, // Edit usernames to look up
+      "user.fields": "created_at,description", // Edit optional query parameters here
+      expansions: "pinned_tweet_id",
+    };
+
+    // this is the HTTP header that adds bearer token authentication
+    const resUserID = await needle("get", endpointURL, paramsUserID, {
+      headers: {
+        "User-Agent": "v2UserLookupJS",
+        authorization: `Bearer ${token}`,
+      },
+    });
+    console.log("resuserıd");
+
+    // if we could find the userId with the given name
+    if (resUserID.body) {
+      const userId = resUserID.body.data[0]["id"]; //assign userId
+      // console.log(resUserID.body);
+      // console.log(userId);
+
+      // Get User Tweet timeline by user ID
+      // https://developer.twitter.com/en/docs/twitter-api/tweets/timelines/quick-start
+      // this is the ID for @TwitterDev
+      // const userId = "2244994945";
+      const url = `https://api.twitter.com/2/users/${userId}/tweets`;
+
+      let userTweets = [];
+
+      // we request the author_id expansion so that we can print out the user name later
+      let paramsUserTweets = {
+        max_results: 100, //limit the max number of tweet to retrieve
+        "tweet.fields": "created_at",
+        expansions: "author_id",
+      };
+
+      // necessary options from the API to validate token to send request
+      const options = {
+        headers: {
+          "User-Agent": "v2UserTweetsJS",
+          authorization: `Bearer ${token}`,
+        },
+      };
+
+      let hasNextPage = true;
+      let nextToken = null;
+      let userName;
+      console.log("Retrieving Tweets...");
+
+      // retrieve tweets from user
+      while (hasNextPage) {
+        const respBody = await needle("get", url, paramsUserTweets, options);
+        let resp = respBody.body;
+        if (
+          resp &&
+          resp.meta &&
+          resp.meta.result_count &&
+          resp.meta.result_count > 0
+        ) {
+          userName = resp.includes.users[0].username;
+          if (resp.data) {
+            userTweets.push.apply(userTweets, resp.data);
+            hasNextPage = false;
+          }
+          if (resp.meta.next_token) {
+            nextToken = resp.meta.next_token;
+            hasNextPage = false;
+          } else {
+            hasNextPage = false;
+          }
+        } else {
+          hasNextPage = false;
+        }
+      }
+
+      console.log(
+        `Got ${userTweets.length} Tweets from ${userName} (user ID ${userId})!`
+      );
+      // helper variable to keep tweets which will be sent to IBM to be analyzed for categories
+      twetoanalyze = "";
+
+      // concatanete each tweets from user to later convert to JSON
+      for (const tweet in userTweets) {
+        var toPush = userTweets[tweet]["text"].replace(/(\r\n|\n|\r)/gm, "");
+        twetoanalyze += toPush;
+      }
+      // conert retrieved tweets to JSON for sending a request to IBM to analyze
+      JSON.stringify(twetoanalyze);
+
+      // assign tweets to text to analyze
+      const analyzeParams = {
+        // text: req.body.toAnalyze,
+        text: twetoanalyze,
+        features: {
+          keywords: {
+            emotion: true,
+          },
+          categories: {
+            emotion: true,
+            sentiment: true,
+            limit: 100,
+            explanation: true,
+          },
+        },
+      };
+      // analyze the given tweets from user
+      const analysisResults = await naturalLanguageUnderstanding.analyze(
+        analyzeParams
+      );
+
+      categories = [];
+      // check for the status and then find and push each categories to array to later find unique categories
+      if (analysisResults["status"] == 200) {
+        for (const key in analysisResults["result"]) {
+          for (
+            let index = 0;
+            index < analysisResults["result"][key].length;
+            index++
+          ) {
+            const element = analysisResults["result"][key][index];
+
+            if (element["label"] != null) {
+              console.log("not null");
+              console.log(element["label"]);
+              var category = element["label"].substr(1).split("/");
+              for (let j = 0; j < category.length; j++) {
+                categories.push(category[j]);
+              }
+            }
+          }
+        }
+        // Obtain Unique Categories
+        let categoriesUnique = categories.filter(
+          (value, index, categories) => categories.indexOf(value) === index
+        );
+        // return response Unique Categories and Analysis Results
+        res.status(201).json({
+          result: categoriesUnique,
+          analysisResults: analysisResults,
+        });
+      } else {
+        res.status(analysisResults["status"]).json({
+          result: analysisResults["statusText"],
+        });
+      }
+    } //if resUserId.body exist
+    else {
+      throw new Error("Unsuccessful request");
+    }
+  } catch (error) {
+    console.log("error:", error);
+    res.status(error["status"]).json({
+      response: "Error Occured",
+      reason: error["name"],
+    });
+  }
+};
